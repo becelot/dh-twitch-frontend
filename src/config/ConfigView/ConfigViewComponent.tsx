ed7548dc308c9d4d4dce2d39b86474248e32d9b2
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-23 11:46:35 +0200

    Basic config style

--- src/config/ConfigView/ConfigViewComponent.tsx
+++ src/config/ConfigView/ConfigViewComponent.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import { Props } from './index';
 
+import * as styles from './ConfigView.scss';
+
 export default class ConfigViewComponent extends React.Component<Props> {
   public componentDidMount(): void {
+    window.Twitch.ext.onContext((context: Partial<TwitchExtContext>) => {});
   }
 
   public render() {
 
     return (
-      <div>
-        <span>
-          {this.props.hasInitialized ? 'hallo' : 'welt'}
-        </span>
+      <div className={styles.wrapper}>
+        <p>Loading setup…</p>
+        <p>This might take a few moments.</p>
       </div>
     );
   }
