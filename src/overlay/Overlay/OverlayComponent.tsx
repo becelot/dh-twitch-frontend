8f9a530e281f77a50d1659d48d3b7d17298a2a81
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-30 19:17:00 +0200

    Basic layout of the overlay

--- src/overlay/Overlay/OverlayComponent.tsx
+++ src/overlay/Overlay/OverlayComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { DeckList } from '../../components/Deck/DeckList';
 import Sidebar from '../../components/Sidebar';
 
 const Wrapper = styled.div`
@@ -10,7 +11,46 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   
-  background: rgba(0,0,0, 0.5);
+  background: rgba(55,88,141, 0.5);
+  user-select: none;
+`;
+
+const Header = styled.h2`
+  position: relative;
+  flex: 0 0 auto;
+  height: 50px;
+  background: linear-gradient(#585958, #585958 10%, #272323 70%, #0C0B0C 95%, #0C0B0C);
+  
+  color: white;
+  text-align: center;
+  text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+  line-height: 44px;
+  
+  border-left: 3px solid #84672D;
+  border-top: 3px solid #EAC884;
+  border-right: 3px solid #84672D;
+  border-bottom: 3px solid #4E391F;
+`;
+
+const DeckListWrapper = styled.div`
+  position: relative;
+  flex: 0 0 auto;
+  height: calc(100% - 200px);
+  width: auto;
+  
+  background: url("https://gamepedia.cursecdn.com/hearthstone_gamepedia/3/31/Thrall-full.jpg?version=cb83492715975917d2416b8832ea9751");
+  background-size: auto calc(100vh - 60px);
+  background-position: -200px -140px;
+  background-repeat: no-repeat;
+  
+  display: flex;
+  justify-content: center;
+  
+`;
+
+const Footer = styled.div`
+  position: relative;
+  flex: 1 0 50px;
 `;
 
 
@@ -19,7 +59,11 @@ export default class extends React.Component {
     return (
       <Sidebar>
         <Wrapper>
-          Hallo Welt
+          <Header>Deck History Tracker</Header>
+          <DeckListWrapper>
+            <DeckList deckList={[{id: 'UNG_035', name: 'Curious Glimmerroot', count: 1}]} deckName={''}/>
+          </DeckListWrapper>
+          <Footer />
         </Wrapper>
       </Sidebar>
     );
