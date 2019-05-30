4e239a78b0009eb6160735965468f8fb1580796a
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-30 20:08:03 +0200

    Added material buttons to header. Header is now the deck title

--- src/overlay/Overlay/OverlayComponent.tsx
+++ src/overlay/Overlay/OverlayComponent.tsx
@@ -1,3 +1,5 @@
+import { ButtonBase, IconButton } from '@material-ui/core';
+import { ArrowRight, ArrowLeft } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 import { DeckList } from '../../components/Deck/DeckList';
@@ -15,7 +17,7 @@ const Wrapper = styled.div`
   user-select: none;
 `;
 
-const Header = styled.h2`
+const Header = styled.div`
   position: relative;
   flex: 0 0 auto;
   height: 50px;
@@ -30,6 +32,25 @@ const Header = styled.h2`
   border-top: 3px solid #EAC884;
   border-right: 3px solid #84672D;
   border-bottom: 3px solid #4E391F;
+  
+  display: flex;
+  flex-direction: row;
+  
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+  
+  div {
+    display: block;
+    flex: 1 1 100%;
+    
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    padding: 0 5px 0 5px;
+  }
 `;
 
 const DeckListWrapper = styled.div`
@@ -59,7 +80,15 @@ export default class extends React.Component {
     return (
       <Sidebar>
         <Wrapper>
-          <Header>Deck History Tracker</Header>
+          <Header>
+            <ButtonBase href={''} style={{borderRadius: '50%', height: '24px', top: '10px'}} title={'Previous'}>
+              <ArrowLeft />
+            </ButtonBase>
+            <div>Deck History Tracker sdfjlkaergh awjle fgjeoöarg er g</div>
+            <ButtonBase href={''} style={{borderRadius: '50%', height: '24px', top: '10px'}} title={'Next'}>
+              <ArrowRight />
+            </ButtonBase>
+          </Header>
           <DeckListWrapper>
             <DeckList deckList={[{id: 'UNG_035', name: 'Curious Glimmerroot', count: 1}]} deckName={''}/>
           </DeckListWrapper>
